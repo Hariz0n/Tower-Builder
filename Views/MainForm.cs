9ddf5f4109b4667483fa639f97228446f7e3871f
Harizon <dp200308@gmail.com>
2022-05-21 12:50:52 +0500

    Renamed Form1 to MainForm

---
+++ Views/MainForm.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace TowerBuilder.Views
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}

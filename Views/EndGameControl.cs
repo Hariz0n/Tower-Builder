9594f45a026b59cc9d36fb76ecff2276f3c330dd
Harizon <dp200308@gmail.com>
2022-05-26 22:16:04 +0500

    New End Game Control

---
+++ Views/EndGameControl.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace TowerBuilder.Views
+{
+    public partial class EndGameControl : UserControl
+    {
+        public EndGameControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
